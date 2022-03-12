ffb987138f6826b1d2c9c5e3082a06a4a48ca061
phuong <phuong@gmail.com>
2022-03-13 02:33:05 +0700

    update code card playlist

--- src/layout/main-content/main-components/discover-components/discover-child-components/HomeRecent.tsx
+++ src/layout/main-content/main-components/discover-components/discover-child-components/HomeRecent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import { CardItem } from "../../../../../until-component/carousel/card-item/CardItem";
+import { CardItem, CardPlayList } from "../../../../../until-component/carousel/card-item/CardItem";
 import { IconPlay } from "./../../../../../until-component/IconList";
 
 const HomeRecent: React.FC<{ title?: string, listCard?: Array<{textContent?: string, imgUrl?: string}>}> = ({ title, listCard }) => {
@@ -24,6 +24,8 @@ const HomeRecent: React.FC<{ title?: string, listCard?: Array<{textContent?: str
             {listCard?.map((card, index)=>(
               <CardItem icon={<IconPlay/>} textContent={card.textContent} imgUrl={card.imgUrl}/>
             ))}
+            <CardPlayList href="/album/Rap-Viet-Cuc-Chat-16-Typh-Tage-Seachains-Lil-Wuyn/ZU6Z07DU.html" 
+            imgUrl="https://photo-resize-zmp3.zadn.vn/w320_r1x1_webp/cover/4/1/7/3/417311b789dbe8a5d8aa82eabf059625.jpg"/>
           </div>
         </div>
       </div>
--- src/until-component/carousel/card-item/CardItem.tsx
+++ src/until-component/carousel/card-item/CardItem.tsx
@@ -1,6 +1,10 @@
 import "./index.scss"
-import React from "react";
-export const CardItem: React.FC<{ icon?: React.ReactNode, imgUrl?: string, textContent?:string }> = ({ icon, imgUrl, textContent }) => {
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import { faEllipsis, faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+export const CardItem: React.FC<{ icon?: React.ReactNode, imgUrl?: string, textContent?: string }> = ({ icon, imgUrl, textContent }) => {
   const playMusic = (event: React.MouseEvent) => {
     const icon = event.target as HTMLElement;
     const containerList = document.querySelectorAll(".top-content");
@@ -11,10 +15,10 @@ export const CardItem: React.FC<{ icon?: React.ReactNode, imgUrl?: string, textC
 
     containerList?.forEach((item, index) => {
       if (item.contains(icon)) {
-        item?.classList?.contains('active')?item?.classList.remove("active"):item?.classList.add("active")
+        item?.classList?.contains('active') ? item?.classList.remove("active") : item?.classList.add("active")
       }
-      if(!item.contains(icon)){
-        const iconPlaying=item.querySelector(".ic-gif-playing-white");
+      if (!item.contains(icon)) {
+        const iconPlaying = item.querySelector(".ic-gif-playing-white");
         item?.classList?.contains('active') && item?.classList.remove("active");
         iconPlaying?.classList.replace("ic-gif-playing-white", "ic-svg-play-circle");
       }
@@ -43,6 +47,80 @@ export const CardItem: React.FC<{ icon?: React.ReactNode, imgUrl?: string, textC
           </div>
           <div className="bot-content">
             <h3 className="title">{textContent}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export const CardPlayList: React.FC<{ href?: string, title?: string, imgUrl?: string }> = ({ href, title, imgUrl }) => {
+  const [isDone, setIsDone] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  function handleAddToLikeList(event: React.MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    const button = event.target as HTMLElement;
+    if (button.classList.contains("done")) {
+      button.classList.remove("done");
+      button.classList.add('active')
+    }
+    else {
+      button.classList.add('done');
+      button.classList.remove("active");
+    }
+    setIsDone((isDone) => !isDone);
+  }
+  function handleMouseOver() {
+    setIsHover(true)
+  }
+  function handleMouseLeave(){
+    setIsHover(false)
+  }
+  return (
+    <div className="carousel-item is-fullhd-1.4 is-widescreen-1.4 is-desktop-2 is-touch-20 is-tablet-20">
+      <div className="playlist-wrapper is-title">
+        <div className="card">
+          <div>
+            <Link title={`${title}`} to={`${href}`} className="link-router">
+              <div className="card-image" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>{/* active className */}
+                <figure className="image is-48x48">
+                  <img src={`${imgUrl}`} alt="" />
+                </figure>
+                {isHover && (
+                  <>
+                    <div className="opacity"></div>
+                    <div className="actions-container">
+                      <div className="box actions playlist-actions">
+                        <button className="btn tooltip-btn animation-like active is-hover-circle button" tabIndex={0} onClick={handleAddToLikeList}>
+                          {isDone ? <FontAwesomeIcon icon={faHeartSolid} className="icon is-like-full" /> : <FontAwesomeIcon icon={faHeartRegular} className="icon" />}
+                          <FontAwesomeIcon icon={faHeartSolid} className="icon is-like-full" />
+                        </button>
+                        <button className="btn action-play button">
+                          <i className="icon action-play ic-svg-play-circle"></i>
+                        </button>
+                        <button className="btn tooltip-btn  active is-hover-circle button" tabIndex={0}>
+                          <FontAwesomeIcon icon={faEllipsis} className="icon-ellipsis" />
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="card-content">
+            <h4 className="title is-6">
+              <Link title={`${title}`} to={`${href}`}>
+                <span>
+                  <span>
+                    <span></span>
+                    <br />
+                    <span></span>
+                  </span>
+                  <span style={{ position: "fixed", visibility: 'hidden', top: 0, left: 0 }}>...</span>
+                </span>
+              </Link>
+            </h4>
           </div>
         </div>
       </div>
